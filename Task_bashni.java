969c7ee93427990f81ba61a2e6a960bcd0bcae43
Igor Kozlov <gofman999@mail.ru>
2022-05-29 17:43:43 +0300

    Дописал решение задачи через рекурсию

--- Task_bashni.java
+++ Task_bashni.java
@@ -8,7 +8,7 @@ public class Task_bashni {
         }
     }*/
 
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         String with = "1";
         String on = "3";
         String some = "2";
@@ -16,10 +16,12 @@ public class Task_bashni {
         Tower(with, on, some, count);
     }
     public static void Tower(String with, String on, String some, int count) {
-        
-    }*/
+      if (count > 1) Tower(with, on, some, count - 1);
+      System.out.printf("[%s] >> [%s] \n", with, on);
+      if (count > 1) Tower(some, on, with, count-1);
+    }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         int n = 0;
         Scanner input = new Scanner(System.in);
         System.out.print("Введите количество дисков:");
@@ -30,28 +32,22 @@ public class Task_bashni {
             System.out.println("Вы ввели не целое число");
         }
         input.close();
-    }
-
-    static void Hanoi(int number) {
-
-    int countDisk, counter = 1, count;
-
-    //System.out.printf("Введите количество дисков: "); /* Ханойская башня */
-    //scanf("%d", &number);
-
-   while (counter <= (int)Math.pow(2, number) - 1) { /* Запускаем цикл повторений */
-
-      if (counter % 2 != 0) { /* На нечетном ходу мы будем трогать только самый маленький диск */
-        System.out.printf("Номер хода: %3d Размер диска: %d ", counter, 1);
-         Tower(number, counter, 1); /* С помощью этой функции определяем для данного диска перемещение */
       }
+   static void Hanoi(int number) {
+   int countDisk, counter = 1, count;
+   //System.out.printf("Введите количество дисков: "); // Ханойская башня
+   //scanf("%d", &number);
 
-      else {  /* Определяем диск который нужно переместить на четном ходу */
+   while (counter <= (int)Math.pow(2, number) - 1) {  // Запускаем цикл повторений
+      if (counter % 2 != 0) {                         // На нечетном ходу мы будем трогать только самый маленький диск
+        System.out.printf("Номер хода: %3d Размер диска: %d ", counter, 1);
+         Tower(number, counter, 1);                   // С помощью этой функции определяем для данного диска перемещение
+      }
+      else {                                          // Определяем диск который нужно переместить на четном ходу
          count = counter;
          countDisk = 0;
-
-         while (count % 2 == 0) {  /* Диск который нужно переместить */
-            countDisk++;           /* будет числом деления номера хода на 2 без остатка */
+         while (count % 2 == 0) {                     // Диск который нужно переместить
+            countDisk++;                              // будет числом деления номера хода на 2 без остатка
             count = count / 2;
          }
          System.out.printf("Номер хода: %3d Размер диска: %d ", counter, countDisk + 1);
@@ -60,32 +56,25 @@ public class Task_bashni {
       counter++;
    }
 }
-
-/* Функция определения перемещения дисков */
-public static void Tower(int n ,int i, int k)
-{
+   // Функция определения перемещения дисков
+   public static void Tower(int n ,int i, int k) {
    int t, axisX, axisY, axisZ;
-
-   if (n % 2 == 0) {  /* Определяем порядок осей в зависимости от четности */
-      axisX = 1;      /* и не четности количества дисков */
+   if (n % 2 == 0) {                                  // Определяем порядок осей в зависимости от четности
+      axisX = 1;                                      // и не четности количества дисков
       axisY = 2;
       axisZ = 3;
    }
-
    else {
       axisX = 1;
       axisY = 3;
       axisZ = 2;
    }
-
-   /* Номер хода можно представить единственным образом */
-   /* как произведение некоего нечетного числа на степень двойки */
-   /* k будет номером диска который мы перемещаем */
+   //Номер хода можно представить единственным образом
+   // как произведение некоего нечетного числа на степень двойки
+   // k будет номером диска который мы перемещаем
    t = ((i / (int)Math.pow(2, k - 1)) - 1) / 2;
-
-   if (k % 2 != 0) {    /* Определяем перемещение дисков  для нечетного хода */
-
-      switch (t % 3) {         /* Выбираем перемещение в зависимости от данного условия */
+   if (k % 2 != 0) {                                  // Определяем перемещение дисков  для нечетного хода
+      switch (t % 3) {                                // Выбираем перемещение в зависимости от данного условия
          case 0:               
          System.out.printf("Перемещаем диск с %d на %d\n", axisX, axisY);
             break;
@@ -97,9 +86,7 @@ public static void Tower(int n ,int i, int k)
             break;
       }
    }
-
-   else {     /* Определяем перемещение дисков  для чётного хода */
-
+   else {                                             // Определяем перемещение дисков  для чётного хода
       switch (t % 3) {
          case 0:
          System.out.printf("Перемещаем диск с %d на %d\n", axisX, axisZ);
@@ -110,7 +97,7 @@ public static void Tower(int n ,int i, int k)
          case 2:
          System.out.printf("Перемещаем диск с %d на %d\n", axisY, axisX);
             break;
+         }
       }
-   }
-}
+   }*/
 }
